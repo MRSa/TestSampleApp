4a90b81f8ca0d06ec1e661692b81e6113c9d801f
MRSa <mrsa@myad.jp>
2023-07-30 22:09:37 +0900

    画面イメージについて、branchから展開。

--- app/src/main/java/net/osdn/ja/gokigen/testsampleapp/MainActivity.kt
+++ app/src/main/java/net/osdn/ja/gokigen/testsampleapp/MainActivity.kt
@@ -5,10 +5,12 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -35,10 +37,14 @@ class MainActivity : AppCompatActivity()
         try
         {
             // ボタンと表示エリアを設定する
-            myDataProvider = MyDataProvider(findViewById(R.id.ip_address), findViewById(R.id.ftp_username), findViewById(R.id.ftp_password))
+            myDataProvider = MyDataProvider(findViewById(R.id.ip_address), findViewById(R.id.ftp_username), findViewById(R.id.ftp_password), findViewById(R.id.checkbox1), findViewById(R.id.checkbox2), findViewById(R.id.checkbox3))
             myListener = MyActionListener(this, myDataProvider, findViewById(R.id.text_information), findViewById(R.id.text_status))
             findViewById<Button>(R.id.btnConnect).visibility = View.INVISIBLE
+            findViewById<Button>(R.id.btnDisconnect).visibility = View.INVISIBLE
             findViewById<Button>(R.id.btnWifiSet).visibility = View.INVISIBLE
+
+            // TextViewの表示エリアをスクロールできるようにする
+            findViewById<TextView>(R.id.text_status).setMovementMethod(ScrollingMovementMethod())
 
             // 権限が確保されているか確認する
             if (allPermissionsGranted())
@@ -64,22 +70,24 @@ class MainActivity : AppCompatActivity()
         try
         {
             // ボタン押下を有効化する
-            val btn1 = findViewById<Button>(R.id.btnConnect)
-            btn1.setOnClickListener(myListener)
-            btn1.setOnLongClickListener(myListener)
-            btn1.visibility = View.VISIBLE
-
-            // ボタン押下を有効化する
-            val btn2 = findViewById<Button>(R.id.btnWifiSet)
-            btn2.setOnClickListener(myListener)
-            btn2.setOnLongClickListener(myListener)
-            btn2.visibility = View.VISIBLE
+            enableButton(R.id.btnConnect)
+            enableButton(R.id.btnDisconnect)
+            enableButton(R.id.btnWifiSet)
 
         }
         catch (e: Exception)
         {
             e.printStackTrace()
         }
+    }
+
+    private fun enableButton(id: Int)
+    {
+        // ボタン押下を有効化する
+        val btn1 = findViewById<Button>(id)
+        btn1.setOnClickListener(myListener)
+        btn1.setOnLongClickListener(myListener)
+        btn1.visibility = View.VISIBLE
     }
 
     private fun allPermissionsGranted() : Boolean
--- app/src/main/java/net/osdn/ja/gokigen/testsampleapp/MyDataProvider.kt
+++ app/src/main/java/net/osdn/ja/gokigen/testsampleapp/MyDataProvider.kt
@@ -1,11 +1,10 @@
 package net.osdn.ja.gokigen.testsampleapp
 
+import android.widget.CheckBox
 import android.widget.EditText
 
-class MyDataProvider(private val address: EditText, private val user: EditText, private val pass: EditText)
+class MyDataProvider(private val address: EditText, private val user: EditText, private val pass: EditText, private val check1: CheckBox, private val check2: CheckBox, private val check3: CheckBox)
 {
-
-
     fun getAddress() : String
     {
         return (address.text.toString())
@@ -18,7 +17,21 @@ class MyDataProvider(private val address: EditText, private val user: EditText, 
 
     fun getPass() : String
     {
-        return (user.text.toString())
+        return (pass.text.toString())
     }
 
+    fun getCheck1() : Boolean
+    {
+        return (check1.isChecked)
+    }
+
+    fun getCheck2() : Boolean
+    {
+        return (check2.isChecked)
+    }
+
+    fun getCheck3() : Boolean
+    {
+        return (check3.isChecked)
+    }
 }
